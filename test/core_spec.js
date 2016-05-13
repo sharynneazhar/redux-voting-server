a94f28d72dc438de986cc2dd20704d1f1d2e77f3
Sharynne Azhar <sharynneazhar@gmail.com>
2016-05-12 19:33:40 -0500

    add the socket.io server package

--- test/core_spec.js
+++ test/core_spec.js
@@ -19,7 +19,7 @@ describe('application logic', () => {
         it('converts to immutable', () => {
             const state = Map();
             const entries = ['Trainspotting', '28 Days Later'];
-            const nextState = setEntries(sate, entries);
+            const nextState = setEntries(state, entries);
 
             expect(nextState).to.equal(Map({
                 entries: List.of('Trainspotting', '28 Days Later')
